d2d9308bc0daf6a6eef35ed366c2fbf730664378
rrodriguez-2023342 <rrodriguez-2023342@kinal.edu.gt>
2026-01-10 13:58:48 -0600

    feat: Se hizo la pagina de Nuevo Contacto y se implementaron metodos para cambiar de paginas

---
+++ javascript/cambioPagina.js
@@ -0,0 +1,9 @@
+// Metodop para cambiar a la pagina de Nuevo Contacto
+function nuevoContacto() {
+    window.location.href = "../html/nuevoContacto.html"
+}
+
+//Metodo para cambiar a la pagina de Contactos
+function contactos() {
+    window.location.href = "../html/contactos.html"
+}
